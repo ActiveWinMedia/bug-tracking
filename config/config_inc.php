e1be37fc00ebe4e8b7fb67b130515bd2d1b9e282
Alex Lund <alexl@activewin.co.uk>
2016-11-18 14:10:28 +0000

    first commit

---
+++ config/config_inc.php
@@ -0,0 +1,15 @@
+<?php
+$g_hostname               = 'db.activewin.co.uk';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'aw_bugs';
+$g_db_username            = 'activewin';
+$g_db_password            = 'lhsad&*23k!hcksdlr[]';
+
+$g_default_timezone       = 'Europe/London';
+
+$g_crypto_master_salt     = 'TOauhIC/Fe9M4Eklf5NyeZKBQLIZ0aWdKgi796suvkI=';
+
+
+/*Don't edit above this line!*/
+
+$g_allow_signup = "disabled";
